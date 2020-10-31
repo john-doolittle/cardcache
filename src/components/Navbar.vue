16a000f5640ada79208da770d28a4eca57e8949d
John Doolittle <jdoo.code@gmail.com>
2020-10-31 16:46:15 -0600

    Added some Navbar items

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,6 +5,11 @@
           <b-navbar-brand href="#">CardCache</b-navbar-brand>
           <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
           <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+              <b-nav-item href="#">Home</b-nav-item>
+              <b-nav-item href="#">Browse Decks</b-nav-item>
+              <b-nav-item href="#">Search</b-nav-item>
+            </b-navbar-nav>
             <!-- Right aligned nav items -->
             <b-navbar-nav class="ml-auto">
               <b-nav-item-dropdown right>
@@ -15,6 +20,11 @@
                 <b-dropdown-item href="#">Settings</b-dropdown-item>
                 <b-dropdown-item href="#">Sign Out</b-dropdown-item>
               </b-nav-item-dropdown>
+              <b-nav-form>
+                <b-button variant="light" type="button">
+                  New Deck
+                </b-button>
+              </b-nav-form>
             </b-navbar-nav>
           </b-collapse>
         </b-container>
