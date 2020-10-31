b61bbcee348c54731cf4d3d77b458b7e33ed37bb
John Doolittle <jdoo.code@gmail.com>
2020-10-31 16:13:39 -0600

    Created Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-container>
+          <b-navbar-brand href="#">CardCache</b-navbar-brand>
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+          <b-collapse id="nav-collapse" is-nav>
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+              <b-nav-item-dropdown right>
+                <!-- Using 'button-content' slot -->
+                <template slot="button-content"><em>User</em></template>
+                <b-dropdown-item href="#">Profile</b-dropdown-item>
+                <b-dropdown-item href="#">Decks</b-dropdown-item>
+                <b-dropdown-item href="#">Settings</b-dropdown-item>
+                <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+              </b-nav-item-dropdown>
+            </b-navbar-nav>
+          </b-collapse>
+        </b-container>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      deck: '',
+    };
+  },
+  methods: {
+  },
+};
+</script>
