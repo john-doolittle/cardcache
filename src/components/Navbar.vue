fff9080ac092c3a83a5aaad2a51ffe3e04209b64
John Doolittle <jdoo.code@gmail.com>
2020-11-01 16:31:45 -0700

    Home page router test

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,7 +6,7 @@
           <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
           <b-collapse id="nav-collapse" is-nav>
             <b-navbar-nav>
-              <b-nav-item href="#">Home</b-nav-item>
+              <b-nav-item to="/">Home</b-nav-item>
               <b-nav-item href="#">Browse Decks</b-nav-item>
               <b-nav-item href="#">Search</b-nav-item>
             </b-navbar-nav>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="home">
+    <h1>Home</h1>
+    <p>Bunch of stuff about the home page, but mostly, it works.</p>
   </div>
 </template>
 
