3f97fe83949f0dbbd94bde8c2aabb228f3014740
John Doolittle <jdoo.code@gmail.com>
2020-11-01 15:42:48 -0700

    Added profile, deck, login views to router index

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import Profile from '../views/Profile.vue';
+import Deck from '../views/Deck.vue';
+import Login from '../views/Login.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +12,21 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile,
+  },
+  {
+    path: '/deck',
+    name: 'Deck',
+    component: Deck,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
   },
 ];
 
