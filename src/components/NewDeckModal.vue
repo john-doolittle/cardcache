97b0b096f557787407f17d35c3fc80ecadaaa53f
John Doolittle <jdoo.code@gmail.com>
2020-11-03 11:52:07 -0700

    Structured modal grid layout

--- src/components/NewDeckModal.vue
+++ src/components/NewDeckModal.vue
@@ -3,39 +3,51 @@
     <template #modal-title>
       Add Deck
     </template>
-    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-      <b-form-group
-        id="input-group-1"
-        v-model="form.name"
-        label="Name"
-        label-for="input-1"
-      >
-        <b-form-input
-          id="input-1"
-          required
-        >
-        </b-form-input>
-      </b-form-group>
+    <b-container>
+      <b-row>
+        <b-col>
+          <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+            <b-form-group
+              id="input-group-1"
+              v-model="form.name"
+              label="Name"
+              label-for="input-1"
+            >
+              <b-form-input
+                id="input-1"
+                required
+              >
+              </b-form-input>
+            </b-form-group>
 
-      <b-form-group id="input-group-2" label="Commander" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          v-model="form.commander"
-          required
-        >
-        </b-form-input>
-      </b-form-group>
-
-      <b-form-group id="input-group-3" label="Format" label-for="input-3">
-        <b-form-select
-          id="input-3"
-          v-model="form.format"
-          :options="formats"
-          required
-        >
-        </b-form-select>
-      </b-form-group>
-    </b-form>
+            <b-row>
+              <b-col>
+                <b-form-group id="input-group-3" label="Format" label-for="input-3">
+                  <b-form-select
+                    id="input-3"
+                    v-model="form.format"
+                    :options="formats"
+                    required
+                  >
+                  </b-form-select>
+                </b-form-group>
+              </b-col>
+              <b-col cols="7">
+                <b-form-group id="input-group-2" label="Commander" label-for="input-2">
+                  <b-form-input
+                  id="input-2"
+                  v-model="form.commander"
+                  required
+                  placeholder="Who is your commander?"
+                  >
+                  </b-form-input>
+                </b-form-group>
+              </b-col>
+            </b-row>
+          </b-form>
+        </b-col>
+      </b-row>
+    </b-container>
     <template #modal-footer>
       <b-button type="reset" variant="outline-danger">Reset</b-button>
       <b-button type="submit" variant="outline-primary">Create</b-button>
