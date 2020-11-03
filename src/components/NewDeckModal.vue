d0554aba72c4f2e53e7820efe30a75d562af5332
John Doolittle <jdoo.code@gmail.com>
2020-11-03 09:52:13 -0700

    Implemented new deck modal

--- src/components/NewDeckModal.vue
+++ src/components/NewDeckModal.vue
@@ -3,9 +3,43 @@
     <template #modal-title>
       Add Deck
     </template>
-    <div class="d-block text-center">
-      <h3>Hello From This Modal!</h3>
-    </div>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        v-model="form.name"
+        label="Name"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          required
+        >
+        </b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Commander" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.commander"
+          required
+        >
+        </b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Format" label-for="input-3">
+        <b-form-select
+          id="input-3"
+          v-model="form.format"
+          :options="formats"
+          required
+        >
+        </b-form-select>
+      </b-form-group>
+    </b-form>
+    <template #modal-footer>
+      <b-button type="reset" variant="outline-danger">Reset</b-button>
+      <b-button type="submit" variant="outline-primary">Create</b-button>
+    </template>
   </b-modal>
 </template>
 
@@ -13,6 +47,32 @@
 export default {
   name: 'NewDeckModal',
   components: {
+  },
+  data() {
+    return {
+      form: {
+        name: '',
+        commander: '',
+        format: null,
+      },
+      formats: ['Brawl', { text: 'Commander', value: null }, 'Historic', 'Legacy', 'Modern', 'Pauper', 'Standard', 'Vintage'],
+      show: true,
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      this.form.name = '';
+      this.form.commander = '';
+      this.form.format = null;
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    },
   },
 };
 </script>
