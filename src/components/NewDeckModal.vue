b1bf78f16e7ed90d63e2c1c30aedc2ed038d8c6b
John Doolittle <jdoo.code@gmail.com>
2020-11-05 13:33:39 -0700

    Restored reset button and commander field

--- src/components/NewDeckModal.vue
+++ src/components/NewDeckModal.vue
@@ -32,12 +32,28 @@
                   </b-form-select>
                 </b-form-group>
               </b-col>
+              <b-col cols="7">
+                <b-form-group
+                  id="input-group-2"
+                  label="Commander"
+                  label-for="input-2"
+                >
+                  <b-form-input
+                  id="input-2"
+                  v-model="form.commander"
+                  required
+                  placeholder="Who is your commander?"
+                  >
+                  </b-form-input>
+                </b-form-group>
+              </b-col>
             </b-row>
           </b-form>
         </b-col>
       </b-row>
     </b-container>
     <template #modal-footer>
+      <b-button type="reset" variant="outline-danger">Reset</b-button>
       <b-button type="submit" variant="outline-primary">Create</b-button>
     </template>
   </b-modal>
@@ -52,6 +68,7 @@ export default {
     return {
       form: {
         name: '',
+        commander: '',
         format: null,
       },
       formats: ['Brawl', { text: 'Commander', value: null }, 'Historic', 'Legacy', 'Modern', 'Pauper', 'Standard', 'Vintage'],
@@ -61,6 +78,16 @@ export default {
   methods: {
     onSubmit(evt) {
       evt.preventDefault();
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      this.form.name = '';
+      this.form.commander = '';
+      this.form.format = null;
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
     },
   },
 };
