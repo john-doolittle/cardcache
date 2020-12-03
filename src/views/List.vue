2e9cc669b4b74f807236978abee862b294bbbcf7
John Doolittle <jdoo.code@gmail.com>
2020-12-03 12:56:09 -0700

    Created List view

---
+++ src/views/List.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="list">
+    <h1>List of Decks</h1>
+    <p>Here, you'll be ablt to view decks that you've built. (eventually)</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  components: {
+  },
+};
+</script>
